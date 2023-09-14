5ed03a62ceff09381dbe3d534fb206593dbe66f7
Carlos Kirui <carloskirui154@gmail.com>
2023-09-14 13:18:35 -0400

    removed unecesarry files

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    static void myMethod() {
+        System.out.print("Welcome to Java");
+
+    }
+
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    // Public method
+    public void myPublicMethod() {
+        System.out.println("Public methods must be called by creating objects");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+
+        Main myObj = new Main();
+        myObj.myPublicMethod();
+    }
+}
